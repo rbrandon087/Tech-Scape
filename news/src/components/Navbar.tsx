3ebe5a0a3c2db0cef4ae29deecdae800e6d33368
Brandon <rbrandon087@gmail.com>
2024-02-19 06:19:27 -0800

    fixing my aler

--- news/src/components/Navbar.tsx
+++ news/src/components/Navbar.tsx
@@ -14,17 +14,6 @@ const Navbar: React.FunctionComponent<INavbarProps> = ({ name }) => {
           <a className="navbar-brand" href="/">
             W.
           </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNavAltMarkup"
-            aria-controls="navbarNavAltMarkup"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
           <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
             <div className="navbar-nav ms-auto">
               <a className="nav-link" href="/">
